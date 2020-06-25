43239f3f2a89061fdf55aaecdda2c12022749de9
Kimberly <you@example.com>
2020-06-25 13:19:37 +0800

    Updates

---
+++ precache-manifest.433ed16cf656c06ff0c814ffad50f416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e19163a67890c666f52d212fde9adb",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "a55f8900c8a2fdfb401b",
+    "url": "/recipe-app/static/css/main.7e8bb483.chunk.css"
+  },
+  {
+    "revision": "7179e048924995549d0d",
+    "url": "/recipe-app/static/js/2.90227f78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.90227f78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a55f8900c8a2fdfb401b",
+    "url": "/recipe-app/static/js/main.2ae9b5b4.chunk.js"
+  },
+  {
+    "revision": "bf5bcd6e03ef1d414f40",
+    "url": "/recipe-app/static/js/runtime-main.47cb3185.js"
+  }
+]);
